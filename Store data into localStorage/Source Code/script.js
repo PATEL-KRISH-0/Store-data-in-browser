2104b35cec1ad86446789b4859aa2111dd48dda4
PATEL KRISH <127637917+PATEL-KRISH-0@users.noreply.github.com>
2023-10-14 15:42:30 +0530

    Add files via upload

---
+++ Store data into localStorage/Source Code/script.js
@@ -0,0 +1,20 @@
+const btn = document.querySelector(".btn");
+const output = document.querySelector(".output");
+const reset = document.querySelector(".reset");
+
+btn.addEventListener("click", () => {
+    const key = document.querySelector(".key").value;
+    const value = document.querySelector(".value").value;
+
+    if ((!key && !value) || (!key || !value)) {
+        alert("Both value required");
+        output.innerHTML = "Error!";
+        return
+    }
+
+    localStorage.setItem(key, value);
+    output.innerHTML = "done!";
+    
+    document.querySelector(".key").value = "";
+    document.querySelector(".value").value = "";
+})
